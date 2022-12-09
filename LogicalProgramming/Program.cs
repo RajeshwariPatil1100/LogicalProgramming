7305c202fe6838669004cacc6db5c01f6743b7f6
RajeshwariPatil1100 <rajeshwaripatil1100@gmail.com>
2022-12-09 19:14:39 +0530

    [Rajeshwari] Fibonacci Series

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LogicalProgramming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming 
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Logical Programming Problems");
+                Console.WriteLine("1.Fibonacci Series" + "\n" + "2.Exit");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        Console.WriteLine("Lets Create Fibonacci Series");
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Fibonacci();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+
+
+            }
+        }
+    }
+}
